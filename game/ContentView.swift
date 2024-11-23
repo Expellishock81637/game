9893d433e660a0b266fc8f003e61c08cb467a324
Expellishock81637 <due93196@gmail.com>
2024-11-23 12:25:45 +0800

    Initial Commit

---
+++ game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  game
+//
+//  Created by Williard Liao on 2024/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by Williard Liao on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
